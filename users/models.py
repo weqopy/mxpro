2028a8c70cd7b87dc81aaee963d712ed8d2f0891
weqopy <weqopy@gmail.com>
2017-12-10 10:30:54 +0800

    update users.models.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,5 @@
+from datetime import  datetime
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -20,3 +22,26 @@ class UserProfile(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class EmailVerifyRecord(models.Model):
+    code = models.CharField(max_length=20, verbose_name='验证码')
+    email = models.EmailField(max_length=50, verbose_name='邮箱')
+    send_type = models.CharField(choices=(('register', '注册'), ('forget', '找回密码')), max_length=10)
+    send_time = models.DateTimeField(default=datetime.now)
+
+    class Meta(object):
+        verbose_name = '邮箱验证码'
+        verbose_name_plural = verbose_name
+
+
+class Banner(models.Model):
+    title = models.CharField(max_length=100, verbose_name='标题')
+    image = models.ImageField(upload_to='banner/%Y/%m', verbose_name='轮播图')
+    url = models.URLField(max_length=200, verbose_name='访问地址')
+    index = models.IntegerField(default=100, verbose_name='顺序')
+    all_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta(object):
+        verbose_name = '轮播图'
+        verbose_name_plural = verbose_name
