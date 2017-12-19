27a16a171f8d330241d957cc9545617ce7789122
weqopy <weqopy@gmail.com>
2017-12-19 22:31:29 +0800

    使用类编写视图函数，使用表单功能提高错误提示效果

--- apps/users/views.py
+++ apps/users/views.py
@@ -2,27 +2,40 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from django.shortcuts import render
 from django.db.models import Q
+from django.views.generic.base import View
+
+from users.forms import LoginForm
 from users.models import UserProfile
 
 
 class CustomBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
-            user = UserProfile.objects.get(Q(username=username)|Q(email=username))
+            user = UserProfile.objects.get(
+                Q(username=username) | Q(email=username))
             if user.check_password(password):
                 return user
         except Exception as e:
             return None
 
-def user_login(request):
-    if request.method == "POST":
-        user_name = request.POST.get('username', "")
-        pass_word = request.POST.get('password', "")
-        user = authenticate(username=user_name, password=pass_word)
-        if user is not None:
-            login(request, user)
-            return render(request, 'index.html')
-        else:
-            return render(request, 'login.html', {"msg":"用户名或密码错误"})
-    elif request.method == "GET":
+
+# 视图函数，类
+class LoginView(View):
+    def get(self, request):
         return render(request, 'login.html', {})
+
+    def post(self, request):
+        # 表单
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            user_name = request.POST.get('username', "")
+            pass_word = request.POST.get('password', "")
+            user = authenticate(username=user_name, password=pass_word)
+            if user is not None:
+                login(request, user)
+                return render(request, 'index.html')
+            else:
+                return render(request, 'login.html',
+                              {"msg": "用户名或密码错误"})
+        else:
+            return render(request, 'login.html', {"login_form": login_form})
