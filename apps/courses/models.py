f493ffea4644e6aa6bef25c0aa350d1ac9fea665
weqopy <weqopy@gmail.com>
2018-01-06 18:12:29 +0800

    机构详情页展示及收藏功能完善

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -2,8 +2,11 @@ from datetime import datetime
 
 from django.db import models
 
+from organization.models import CourseOrg
+
 
 class Course(models.Model):
+    course_org = models.ForeignKey(CourseOrg, verbose_name='课程机构', null=True)
     name = models.CharField(max_length=50, verbose_name='课程名')
     desc = models.CharField(max_length=300, verbose_name='课程描述')
     detail = models.TextField(verbose_name='课程详情')
--- apps/organization/models.py
+++ apps/organization/models.py
@@ -45,6 +45,8 @@ class Teacher(models.Model):
     points = models.CharField(max_length=50, verbose_name='教学特点')
     click_nums = models.IntegerField(default=0, verbose_name='点击数')
     fav_nums = models.IntegerField(default=0, verbose_name=' 收藏数')
+    image = models.ImageField(default='', upload_to='courses/%Y/%m', verbose_name='头像')
+
     add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
 
     class Meta:
