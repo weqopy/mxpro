f8be864e99560a3801619140dad87cc2a6d9517d
weqopy <weqopy@gmail.com>
2018-02-10 18:05:32 +0800

    New changelist test of github in pycharm

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 
-from .views import UserInfoView, UploadImageView, SendEmailCodeView, \
-    UpdateEmailView
+from .views import UserInfoView, UploadImageView, UpdateEmailView
 
 urlpatterns = [
     # 用户信息
@@ -14,7 +13,6 @@ urlpatterns = [
 
     # TODO: 发送过程中邮箱验证失败，通过 debug 获取 code 修改成功
     # 发送邮箱验证码
-    url(r'^sendemail_code/$', SendEmailCodeView.as_view(), name='sendemail_code'),
 
     # 修改邮箱
     url(r'^update_email/$', UpdateEmailView.as_view(), name='update_email'),
